628346bdd1e2e77522462d80233c34e490d656a9
Tahiya07 <tahiyazareen2020@gmail.com>
2025-04-13 00:02:03 +0600

    v 0.0.11 - working on faculty page 1.0

--- uap_cse/urls.py
+++ uap_cse/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('faculty/', views.faculty, name='faculty'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- uap_cse/views.py
+++ uap_cse/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+def faculty(request):
+    return render(request, 'Faculty.html')
