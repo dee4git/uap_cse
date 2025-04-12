a9eb36776495ffc96448e39ed94d44d53583353a
Durjoy <dee4code@gmail.com>
2025-04-13 02:07:07 +0600

    v 0.1.7- Durjoy
    Logs:
    - faculty page work in progress

--- uap_cse/views.py
+++ uap_cse/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 def faculty(request):
-    return render(request, 'Faculty.html')
+    return render(request, 'faculty.html')
