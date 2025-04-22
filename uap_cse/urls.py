f4e0c4d8ecf1d76ef15c5cd9e1ecfe82759f9aa8
Durjoy <dee4code@gmail.com>
2025-04-22 23:22:26 +0600

    v 0.3.0- Durjoy
    Logs:
    - media can be added now

--- uap_cse/urls.py
+++ uap_cse/urls.py
@@ -33,3 +33,5 @@ urlpatterns = [
     path('gallery/', views.gallery, name='gallery'),
     path('club_detail/', views.club_detail, name='club_detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
