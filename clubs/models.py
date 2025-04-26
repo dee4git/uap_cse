6a049902958f2eb15e120e6f6aa68be335ab1ea1
Durjoy <dee4code@gmail.com>
2025-04-27 03:18:07 +0600

    v 0.4.8 - Durjoy
    Logs:
    - club models ready with default pics

--- clubs/models.py
+++ clubs/models.py
@@ -8,14 +8,68 @@ from django.contrib.auth.models import User
 from faculty.models import Faculty
 
 
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+import os
+
 class Club(models.Model):
-    convener = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True, blank=True)  # optional
+    convener = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=100)
-    logo = models.ImageField(upload_to='club_logos/')
+    logo = models.ImageField(upload_to='club_logos/', default='defaults/faculty.png')
     moto = models.CharField(max_length=255)
-    cover_picture = models.ImageField(upload_to='club_covers/')
+    cover_picture = models.ImageField(upload_to='club_covers/', default='defaults/clubcover.png')
     description = models.TextField()
-    status = models.BooleanField(default=False)  # default boolean false
+    status = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        # Auto-process logo (1:1)
+        if self.logo:
+            logo_path = self.logo.path
+            img = Image.open(logo_path)
+
+            width, height = img.size
+            min_dim = min(width, height)
+            left = (width - min_dim) / 2
+            top = (height - min_dim) / 2
+            right = (width + min_dim) / 2
+            bottom = (height + min_dim) / 2
+            img_cropped = img.crop((left, top, right, bottom))
+
+            img_resized = img_cropped.resize((300, 300))  # You can adjust final size
+            img_resized.save(logo_path, optimize=True, quality=85)
+
+        # Auto-process cover picture (21:9)
+        if self.cover_picture:
+            cover_path = self.cover_picture.path
+            img = Image.open(cover_path)
+
+            width, height = img.size
+            target_ratio = 21 / 9
+
+            # Calculate new cropping box
+            current_ratio = width / height
+
+            if current_ratio > target_ratio:
+                # Too wide, crop sides
+                new_width = int(height * target_ratio)
+                left = (width - new_width) / 2
+                top = 0
+                right = (width + new_width) / 2
+                bottom = height
+            else:
+                # Too tall, crop top/bottom
+                new_height = int(width / target_ratio)
+                left = 0
+                top = (height - new_height) / 2
+                right = width
+                bottom = (height + new_height) / 2
+
+            img_cropped = img.crop((left, top, right, bottom))
+            img_resized = img_cropped.resize((1680, 720))  # 21:9 resolution, optional
+            img_resized.save(cover_path, optimize=True, quality=85)
 
     def __str__(self):
         return self.name
