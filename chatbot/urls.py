66163d105464f7bcf6afd411f025383ce4f4065c
Taj Mohammad Anim <143492116+Anim36@users.noreply.github.com>
2025-05-04 15:10:29 +0600

    Delete chatbot directory

--- chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home , name='chat'),  # frontend
-    path('get_response/', views.get_response),  # backend API
-]
